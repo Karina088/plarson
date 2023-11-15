17f3e103503f1434171c860a508ab269d9b1b3f1
Kary <morskya.life@gmail.com>
2023-11-15 15:04:00 +0300

    added solutions to tasks

---
+++ 3/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+window.addEventListener('load', () => {
+    console.log('Страница полностью загружена!')
+})
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     console.log('DOM готов!')
+// })
+
